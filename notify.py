a56c745e28e8047fb4591ea7dfb264226a879817
Dhruv Pandya <drp2595@gmail.com>
2021-04-28 16:13:24 +0530

    TimeBasedNotification
    Added functionality to read details and time to notify from excel sheet.

--- notify.py
+++ notify.py
@@ -1,11 +1,34 @@
 import datetime
-from time import sleep
 from plyer import notification #for getting notification on your PC
+import pandas as pd
+import xlrd
+import pause
 
-while(True):
-    notification.notify(
-        title = "Time is"+str(datetime.datetime.now().strftime("%c")), #formated datetim 'Wed Apr 28 11:26:17 2021' <- look like this
-        message = "Time To Remind Something",
+
+wb = xlrd.open_workbook('Sample.xlsx') #Load xl file
+sheet = wb.sheet_by_index(0) #Reading details from first Sheet
+counter=1  #raw counter
+while (True):
+	
+	#Try block for index errors
+	try:
+		row=sheet.row_values(counter) #Skipping title row, starting from second row
+		Title=row[0]                  #Column 1
+		Detail=row[1]                 #Column 2
+		Hour=int(row[2])              #Column 3
+		Min=int(row[3])               #Column 4
+		Sec=int(row[4])               #Column 5
+		untilTime=datetime.datetime(2021, 4, 28, Hour,Min,Sec) #Converting hour,minute,second to milisecond.
+		print(untilTime)
+		print('\n')
+		pause.until(untilTime)  #this function will pause the execution till 'untilTime' miliseconds from starting.
+		#below is notification function.
+		notification.notify(
+        title = Title ,
+        message = Detail + '\n' + str(Hour)+':'+str(Min)+':'+str(Sec),
         timeout  = 20 # the notification stays for 'timeout' sec
-    )
-    sleep(60*60*12)  # 60 sec * 60 min * 12 hours so 60*60*12.... notification on every 12 hours
+    	)
+	except:
+		print("Finished")
+		break
+	counter=counter+1
