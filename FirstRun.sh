3408529106d2553be4a29837dd4ca1f6f3734970
dhruv-spot <dhruvpandya.spot@gmail.com>
2025-03-05 13:40:48 +0530

    entire code change as per lots of issue appear

---
+++ FirstRun.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+pip3 install plyer
+
+pip3 install pause
+
+pip install pandas
+
+pip3 install xlrd
--- notify.py
+++ notify.py
@@ -1,34 +1,51 @@
+import csv
+import time
 import datetime
-from plyer import notification #for getting notification on your PC
-import pandas as pd
-import xlrd
-import pause
+import subprocess
 
+def send_notification(title, message):
+    subprocess.run(["notify-send", title, message])
 
-wb = xlrd.open_workbook('Sample.xlsx') #Load xl file
-sheet = wb.sheet_by_index(0) #Reading details from first Sheet
-counter=1  #raw counter
-while (True):
-	
-	#Try block for index errors
-	try:
-		row=sheet.row_values(counter) #Skipping title row, starting from second row
-		Title=row[0]                  #Column 1
-		Detail=row[1]                 #Column 2
-		Hour=int(row[2])              #Column 3
-		Min=int(row[3])               #Column 4
-		Sec=int(row[4])               #Column 5
-		untilTime=datetime.datetime(2021, 4, 28, Hour,Min,Sec) #Converting hour,minute,second to milisecond.
-		print(untilTime)
-		print('\n')
-		pause.until(untilTime)  #this function will pause the execution till 'untilTime' miliseconds from starting.
-		#below is notification function.
-		notification.notify(
-        title = Title ,
-        message = Detail + '\n' + str(Hour)+':'+str(Min)+':'+str(Sec),
-        timeout  = 20 # the notification stays for 'timeout' sec
-    	)
-	except:
-		print("Finished")
-		break
-	counter=counter+1
+def wait_until(target_time):
+    while True:
+        now = datetime.datetime.now()
+        if now >= target_time:
+            break
+        time.sleep(10)
+
+def read_csv_and_schedule(filename):
+    try:
+        with open(filename, newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip header
+
+            for row in reader:
+                if len(row) != 4:
+                    continue
+
+                title, message, event_time, before_minutes = row
+                
+                try:
+                    event_time = datetime.datetime.strptime(event_time, "%H:%M")
+                    before_minutes = int(before_minutes)
+                    
+                    now = datetime.datetime.now()
+                    event_datetime = now.replace(hour=event_time.hour, minute=event_time.minute, second=0, microsecond=0)
+
+                    notify_time = event_datetime - datetime.timedelta(minutes=before_minutes)
+                    if notify_time < now:
+                        print(f"Skipping past notification: {title} at {notify_time.strftime('%H:%M')}")
+                        continue
+                    
+                    print(f"Scheduled notification: {title} at {notify_time.strftime('%H:%M')}")
+                    wait_until(notify_time)
+                    send_notification(title, message)
+
+                except ValueError:
+                    print(f"Invalid time format in row: {row}")
+
+    except FileNotFoundError:
+        print("CSV file not found!")
+
+if __name__ == "__main__":
+    read_csv_and_schedule("data.csv")
